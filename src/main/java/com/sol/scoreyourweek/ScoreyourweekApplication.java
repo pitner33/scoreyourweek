49a56839cb3fcdd8a8eb45fda4476fccfe886b62
Peter Kovacs <peter.kovacs@dpc.hu>
2019-08-02 15:25:32 +0200

    initial

---
+++ src/main/java/com/sol/scoreyourweek/ScoreyourweekApplication.java
@@ -0,0 +1,13 @@
+package com.sol.scoreyourweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreyourweekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreyourweekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sol/scoreyourweek/ScoreyourweekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sol.scoreyourweek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoreyourweekApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
